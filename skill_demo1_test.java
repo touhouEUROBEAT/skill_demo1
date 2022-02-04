56d7d9ccf6fd71c62165abf9f63db94972ede571
JT <spaike97@DESKTOP-HJ9KD4Q.lan>
2022-02-04 00:37:52 -0800

    .

--- skill_demo1_test.java
+++ skill_demo1_test.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class skill_demo1_test {
     @Test
     public void foo2() {
-        assertFalse(skill_demo1.foo());
+        assertTrue(skill_demo1.foo());
     }
 }
